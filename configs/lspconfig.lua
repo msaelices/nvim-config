c8fd54bc9bb1f3a4bdf18f59d154dbd6182b7239
Manuel Saelices <msaelices@gmail.com>
2024-03-28 00:16:50 +0100

    Configure LSPs

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,15 +5,37 @@ local capabilities = config.capabilities
 
 local lspconfig = require("lspconfig")
 
-local servers = {
-  "pyright",
-  "ruff_lsp",
-}
+lspconfig.ruff_lsp.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  autoformat = true,
+  filetypes = {"python"},
+  init_options = {
+      -- https://github.com/astral-sh/ruff-lsp#settings
+      -- https://github.com/astral-sh/ruff-lsp/blob/main/ruff_lsp/server.py
+      settings = {
+        fixAll = true,
+        organizeImports = false,  -- let isort take care of organizeImports
+        -- extra CLI arguments
+        -- https://beta.ruff.rs/docs/configuration/#command-line-interface
+        -- https://beta.ruff.rs/docs/rules/
+        args = {
+          "--config", "~/.config/nvim/lua/custom/configs/ruff.toml",
+          "--ignore", table.concat({
+          "E111", -- indentation-with-invalid-multiple
+          "E114", -- indentation-with-invalid-multiple-comment
+          "E402", -- module-import-not-at-top-of-file
+          "E501", -- line-too-long
+          "E702", -- multiple-statements-on-one-line-semicolon
+          "E731", -- lambda-assignment
+        }, ',') },
+      },
+  }
+})
 
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = {"python"},
-  })
-end
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
