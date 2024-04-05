89af9c98fff8be0d3782afb471bb52ed104a572d
Manuel Saelices <msaelices@gmail.com>
2024-04-05 22:19:05 +0200

    Remove two keymaps not supported in python

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -234,12 +234,10 @@ local plugins = {
            enable = true,
            swap_next = {
              ["<leader>na"] = "@parameter.inner", -- swap parameters/argument with next
-             ["<leader>n:"] = "@property.outer", -- swap object property with next
              ["<leader>nm"] = "@function.outer", -- swap function with next
            },
            swap_previous = {
              ["<leader>pa"] = "@parameter.inner", -- swap parameters/argument with prev
-             ["<leader>p:"] = "@property.outer", -- swap object property with prev
              ["<leader>pm"] = "@function.outer", -- swap function with previous
            },
          },
