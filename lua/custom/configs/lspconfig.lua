faa789c4e66cb22cd613e6bf11a57637eabe45c3
Manuel Saelices <msaelices@gmail.com>
2024-10-12 23:09:12 +0200

    Proper Mojo LSP support, once is supported by nvim-lspconfig

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -43,5 +43,5 @@ lspconfig.vuels.setup({
 lspconfig.mojo.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = {"conf", "mojo"},  -- The mojo filetype is not recognized as mojo but as conf
+    filetypes = {"mojo"},  -- The mojo filetype is not recognized as mojo but as conf
 })
