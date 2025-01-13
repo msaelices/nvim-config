ece1afba2bcf0c6e3f30ceea81b82cc2129a0e5a
Manuel Saelices <msaelices@gmail.com>
2025-01-13 12:18:06 +0100

    Add previous LSP config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -16,9 +16,45 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+lspconfig.ruff.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = nvlsp.capabilities,
+  autoformat = true,
+  filetypes = { "python" },
+  init_options = {
+    -- https://github.com/astral-sh/ruff-lsp#settings
+    -- https://github.com/astral-sh/ruff-lsp/blob/main/ruff_lsp/server.py
+    settings = {
+      fixAll = true,
+      organizeImports = true,
+      logLevel = "debug",
+      -- extra CLI arguments
+      -- https://docs.astral.sh/ruff/configuration/#command-line-interface
+      format = {
+        args = {
+          "--config",
+          "~/.config/nvim/lua/configs/ruff.toml",
+        },
+      },
+    },
+  },
+}
+
+lspconfig.pyright.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = nvlsp.capabilities,
+  filetypes = { "python" },
+}
+
+lspconfig.vuels.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = nvlsp.capabilities,
+  filetypes = { "vue" },
+}
+
+lspconfig.mojo.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = nvlsp.capabilities,
+  filetypes = { "mojo" }, -- The mojo filetype is not recognized as mojo but as conf
+  autoformat = true,
+}
