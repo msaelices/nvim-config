177c7b202920067bd9ec3dc137df48d88154bdbd
Manuel Saelices <msaelices@gmail.com>
2025-01-14 22:18:28 +0100

    Add previous options to the new config

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,36 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
+
+o.autoread = true
+o.nu = true
+o.relativenumber = true
+
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+
+o.smartindent = true
+
+o.wrap = false
+o.endofline = false
+
+o.swapfile = false
+o.backup = false
+o.undodir = os.getenv "HOME" .. "/.vim/undodir"
+o.undofile = true
+
+o.hlsearch = false
+o.incsearch = true
+
+o.termguicolors = true
+
+o.scrolloff = 8
+o.signcolumn = "yes"
+
+o.updatetime = 50
+
+o.colorcolumn = "88"
